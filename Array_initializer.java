942cc4cb48e804470c9034df0f11c729fb5a272e
Siraj Dujana <148343489+dujana07@users.noreply.github.com>
2023-10-18 21:32:18 +0500

    Array initializer
    Initialize the array and traverse the elements of the Array.

---
+++ Array_initializer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays.in.java;
+
+public class Array_initializer {
+    public static void main(String[] args) {
+        //Array initializer
+        int array[]={1,2,3,4,5};
+        //Traversing an element
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
