6ef1daf2ed66ddd3d68d9a931059ccabae2f9b7a
Siraj Dujana <148343489+dujana07@users.noreply.github.com>
2023-10-22 16:07:18 +0500

    Array
    Traversing the elements of array using foreach loop

---
+++ traversing_elements_of_array_using_foreach_loop.java
@@ -0,0 +1,11 @@
+
+package arrays.in.java;
+
+public class traversing_elements_of_array_using_foreach_loop {
+    public static void main(String[] args) {
+        int array[]={1,2,3,4,5,6};
+        for(int item:array){
+            System.out.println(item);
+        }
+    }
+}
