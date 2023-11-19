0c1a1cf8c112048b44abe39a005c44dcdbbab1d3
Siraj Dujana <148343489+Siraj-Dujana@users.noreply.github.com>
2023-11-19 22:43:18 +0500

    Update Average_value_of_array_elements.java

--- Average_value_of_array_elements.java
+++ Average_value_of_array_elements.java
@@ -4,7 +4,7 @@ public class Average_value_of_array_elements {
     public static void main(String[] args) {
         int array[]={1,2,3,4,5,6};
         int count=0;
-        int sum=0;
+        double sum=0;
         for(int i=0;i<array.length;i++){
             sum+=array[i];
             count++;
