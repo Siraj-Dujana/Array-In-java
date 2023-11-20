355567c4947117c21de6b74d69e1954ecbb37206
Siraj Dujana <148343489+Siraj-Dujana@users.noreply.github.com>
2023-11-20 19:35:29 +0500

    Find Multiple of 3 or 5
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get and 3,5,6 and 9 .The sum of these multiples is 23.
    Find the sum of all the multiples 3 of or 5 below N .
    
    
    Sample Input 0
    2
    10
    100
    
    Sample Output 0
    23
    2318

---
+++ Euler_Multiples_of_3_and_5.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays.in.java;
+
+/**
+ *
+ * @author Khushkuro
+ */
+public class Euler_Multiples_of_3_and_5 {
+    
+}
